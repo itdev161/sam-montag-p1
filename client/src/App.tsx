401ded6529ebdab7f8cbe3fe49d429c3a74a4d51
samtheman43 <montags@gmatc.matc.edu>
2020-10-21 21:15:59 -0500

    First Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function Root() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Welcome-message">
+          <h2>Welcome To React!!</h2>
+        </div>
+        <img id="earthlogo" src="https://www.uidownload.com/files/442/743/719/globe-browser-earth-world-planet-icon.png" className="App-logo" alt="logo" />
+        <p>This is our Earth. The Earth rotates at a speed of roughly 1,000 miles per hour!</p> 
+        
+      
+      </header>
+    </div>
+  );
+}
+
+export default Root;
